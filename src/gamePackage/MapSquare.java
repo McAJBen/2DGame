6a01f64b521752a5b11094035ac6f405d939cd4d
Benjamin <McAJBen@gmail.com>
2016-01-28 21:50:25 -0500

    aligning MapSquares

--- src/gamePackage/MapSquare.java
+++ src/gamePackage/MapSquare.java
@@ -34,32 +34,42 @@ public class MapSquare {
 
 	public static void paint(MapSquare[][] mapSquares, Graphics g, Dimension screenSize) {
 		
-		
-		
 		Dimension squareSize = new Dimension(
 				screenSize.width / GLOBAL.MAP_PIXEL_SIZE,
 				screenSize.height / GLOBAL.MAP_PIXEL_SIZE);
 		
+		Dimension offSet = new Dimension(
+				screenSize.width - squareSize.width * GLOBAL.MAP_PIXEL_SIZE,
+				screenSize.height - squareSize.height * GLOBAL.MAP_PIXEL_SIZE);
+				
+		
 		for (int i = 0; i < GLOBAL.MAP_PIXEL_SIZE; i++) {
 			for (int j = 0; j < GLOBAL.MAP_PIXEL_SIZE; j++) {
+				 // TODO fix this
+				Dimension thisSquareSize = new Dimension(
+						squareSize.width + ((i % offSet.width == 0) ? 1 : 0),
+						squareSize.height + ((i % offSet.height == 0) ? 1 : 0));
+				
+				
+				
 				
 				g.setColor(mapSquares[i][j].getColor());
 				
 				switch (mapSquares[i][j].getType()) {
 				case COIN:
 					g.fillRect(
-							i * squareSize.width, j * squareSize.height,
-							squareSize.width, squareSize.height);
+							i * thisSquareSize.width, j * thisSquareSize.height,
+							thisSquareSize.width, thisSquareSize.height);
 					g.setColor(COIN_COLOR);
 					g.fillOval(
-						i * squareSize.width, j * squareSize.height,
-						squareSize.width, squareSize.height);
+						i * thisSquareSize.width, j * thisSquareSize.height,
+						thisSquareSize.width, thisSquareSize.height);
 					break;
 				case FLOOR:
 				case WALL:
 					g.fillRect(
-						i * squareSize.width, j * squareSize.height,
-						squareSize.width, squareSize.height);
+						i * thisSquareSize.width, j * thisSquareSize.height,
+						thisSquareSize.width, thisSquareSize.height);
 					break;
 				
 				}
